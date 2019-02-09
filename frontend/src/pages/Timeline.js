825a50c06b2ded4297cc382cc4785b46e05bb3ee
Victor Almeida <victor.freitas08@hotmail.com>
2019-02-09 12:59:27 -0200

    aplicaçâo react.js terminada

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import api from '../services/api.js';
+import socket from "socket.io-client";
 
 import './Timeline.css'
 import twitterLogo from '../twitter.svg';
+import Tweet from "../components/Tweet";
 
 
 export default class Timeline extends Component {
 
   state ={ 
+    tweets : [],
     newTwitte: '',
   };
+
+  async componentDidMount(){
+
+    this.subscribeToEvents();
+
+    const response = await api.get("tweets");
+
+    this.setState({tweets: response.data});
+  }
+
+
+  subscribeToEvents = () =>{
+    const io = socket("http://localhost:3000");
+
+    io.on("tweet",data =>{
+      this.setState({tweets : [data, ...this.state.tweets]});
+    });
+
+    io.on("like",data =>{
+      this.setState({tweets: this.state.tweets.map(tweet => (
+        tweet._id === data._id ? data : tweet)
+        )});
+    });
+  }
+
 
   handleNewTwitte = async(e) =>{
     if(e.keyCode !== 13) return;
@@ -38,9 +66,11 @@ export default class Timeline extends Component {
           placeholder="O que está acontecendo?"
         />
       </form>
-      <ul>
-
-      </ul>
+        <ul className="tweet-list">
+            {this.state.tweets.map(tweet => (
+               <Tweet key={tweet._id} tweet={tweet}/>
+             ))}
+        </ul>
       </div>
     );
   }
